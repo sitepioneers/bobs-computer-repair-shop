f3ce5e6a4a3f36eb69581dfc9dba55ce7b0539ba
April Auger <april@sitepioneers.com>
2020-04-11 06:46:28 -0700

    Added code attribution

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,3 +1,10 @@
+/*
+ *  Title: nav.component.ts
+ *  Author: April Auger, Wendy Portillo, Thip Rattanavilay
+ *  Date: 11 April 2020
+ *  Description: The nav component for the BCRS application.
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthorizationService } from '../services/authorization.service';
