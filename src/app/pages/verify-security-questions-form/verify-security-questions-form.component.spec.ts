8fd4dc1c8a75eb875f699c1535a9baf5d6e3ae84
Thip Rattanavilay <trattanavilay@my365.bellevue.edu>
2020-04-18 18:41:20 -0700

    Create verify security questions form (forgot password) process

---
+++ src/app/pages/verify-security-questions-form/verify-security-questions-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerifySecurityQuestionsFormComponent } from './verify-security-questions-form.component';
+
+describe('VerifySecurityQuestionsFormComponent', () => {
+  let component: VerifySecurityQuestionsFormComponent;
+  let fixture: ComponentFixture<VerifySecurityQuestionsFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VerifySecurityQuestionsFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VerifySecurityQuestionsFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
