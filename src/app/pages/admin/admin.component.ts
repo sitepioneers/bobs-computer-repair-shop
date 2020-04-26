a8126c08e5d2d69d25cf2df4beeb4a3000ff8884
Thip Rattanavilay <trattanavilay@my365.bellevue.edu>
2020-04-26 03:56:46 -0700

    Created service repair, admin, user map, components

---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,23 @@
+/*
+ *  Title: admin.ts
+ *  Author: Thip Rattanavilay
+ *  Date: 26 April 2020
+ *  Description: The invoice API for the BCRS application.
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
