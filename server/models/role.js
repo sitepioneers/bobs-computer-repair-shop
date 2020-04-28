ae8087c9f1745a0f9a55a0ed605c4aa2bfed8755
dearwendy <dearwendy714@gmail.com>
2020-04-27 21:34:04 -0700

    contact page,footer,banner

--- server/models/role.js
+++ server/models/role.js
@@ -1,14 +1,16 @@
 /*
 Title: role.js
-Author: Professor Krasso
+Author: April Auger, Wendy Portillo, Thip Rattanavilay
 Date: April 20, 2020
 Description: Model for MongoDB ROles collection
 */
 
-
+//import libraries
 const mongoose = require('mongoose');
 
+//blue print definition
 let roleSchema = mongoose.Schema({
-    text: {type: String, unique: true, dropDups: true}
+    text: {type: String, unique: true, dropDups: true} //unique does not automatically act like a validator so it will not auto throw an error if dup entry exists
 });
-module.exports = mongoose.model('Role', roleSchema);
+module.exports = mongoose.model('Role', roleSchema); //now a model is needed
+
